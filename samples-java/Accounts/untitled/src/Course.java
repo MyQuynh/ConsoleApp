53f20c59b6f18bc4b8ada3cce314f4c11a105274
MyQuynh <ngoquynh8383@gmail.com>
2021-03-18 08:59:49 +0700

    Creating constructor for Course class and also the getter and setter method

--- samples-java/Accounts/untitled/src/Course.java
+++ samples-java/Accounts/untitled/src/Course.java
@@ -2,4 +2,34 @@ public class Course {
     private String id;
     private String name;
     private int number_of_credits;
+
+    public Course(String id, String name, int number_of_credits) {
+        this.id = id;
+        this.name = name;
+        this.number_of_credits = number_of_credits;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber_of_credits() {
+        return number_of_credits;
+    }
+
+    public void setNumber_of_credits(int number_of_credits) {
+        this.number_of_credits = number_of_credits;
+    }
 }
