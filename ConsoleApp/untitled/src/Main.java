3d49c9c27e117315abf7aafc99b30d403896545b
Quynh <ngoquynh8383@gmail.com>
2021-03-21 22:35:37 +0700

    Making some dummy data of courses and students

--- ConsoleApp/untitled/src/Main.java
+++ ConsoleApp/untitled/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -23,7 +24,6 @@ public class Main {
             // Get the option from user
             System.out.print("Your option: ");
             String input = Main.scanner.nextLine();
-            System.out.println("\n");
 
             if (input.equals("1")){
 
@@ -41,7 +41,6 @@ public class Main {
 
                     // Get the input from the users
                     input = Main.scanner.nextLine();
-                    System.out.println("\n");
                     if (input.equals("1")){
                         studentEnrollment.add();
                     } else if (input.equals("2")) {
@@ -66,6 +65,62 @@ public class Main {
     }
 
     public static void main(String[] args){
-        menu();
+
+        // Creating a list of courses and add it to the Courses array
+        CourseManager courseManager = new CourseManager();
+
+        Course course1 = new Course("COSC2083","Introduction to Information Technology", 12);
+        Course course2 = new Course("COSC2429","Introduction to Programming", 12);
+        Course course3 = new Course("ISY3414","Practical Database Concept", 12);
+        Course course4 = new Course("COSC2500", "Intro to Computer Systems", 12);
+        Course course5 = new Course("COSC2534","Building IT Systems", 12);
+        Course course6 = new Course("COSC2652", "User-central Design", 12);
+        Course course7 = new Course("COSC2430", "Web Programming", 12);
+        Course course8 = new Course("COSC2081","Programming 1", 12);
+        Course course9 = new Course("COSC2440", "Software Architecture: Design and Implementation", 12);
+        Course course10 = new Course("COSC2539","Security in Computing and Information Technology", 12);
+
+        courseManager.add(course1);
+        courseManager.add(course2);
+        courseManager.add(course3);
+        courseManager.add(course4);
+        courseManager.add(course5);
+        courseManager.add(course6);
+        courseManager.add(course7);
+        courseManager.add(course8);
+        courseManager.add(course9);
+        courseManager.add(course10);
+
+        System.out.println(courseManager.toString());
+
+        // Making a list of student and store it in the student Array
+        DateManager dateManager = new DateManager();
+        StudentManager studentManager = new StudentManager();
+
+        Student student1 = new Student("S3836322","Ngo My Quynh",dateManager.convertDateString("08/03/2001"));
+        Student student2 = new Student("S3817852","Nguyen Thanh Thien",dateManager.convertDateString("06/11/2000"));
+        Student student3 = new Student("S3836455","Nguyen Thanh Tuan",dateManager.convertDateString("14/12/1995"));
+        Student student4 = new Student("S3818102","Pham Cong Minh",dateManager.convertDateString("05/05/2001"));
+        Student student5 = new Student("S3818381","Vo Tran Truong Duy",dateManager.convertDateString("08/11/2001"));
+        Student student6 = new Student("S3754105","Nguyen Dang Lam Phuong",dateManager.convertDateString("25/07/2000"));
+        Student student7 = new Student("S3812649","Nguyen Pham Quoc Minh",dateManager.convertDateString("24/08/2001"));
+        Student student8 = new Student("S3740853","Vu Phuong Thao",dateManager.convertDateString("18/09/1999"));
+        Student student9 = new Student("S3804690","Nguyen Ngoc Dang Hung",dateManager.convertDateString("20/08/2000"));
+        Student student10 = new Student("S3689902","Nguyen Cao Minh Duc",dateManager.convertDateString("21/02/1999"));
+
+        studentManager.add(student1);
+        studentManager.add(student2);
+        studentManager.add(student3);
+        studentManager.add(student4);
+        studentManager.add(student5);
+        studentManager.add(student6);
+        studentManager.add(student7);
+        studentManager.add(student8);
+        studentManager.add(student9);
+        studentManager.add(student10);
+
+        System.out.println(studentManager.toString());
+
+        //menu();
     }
 }
