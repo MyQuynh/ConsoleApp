4faaf7d852b58201e1405b4540111cede9cab1dc
Quynh <ngoquynh8383@gmail.com>
2021-03-22 00:21:09 +0700

    Making change in StudentManager.java: Moving dummy data of Student list to this class

--- ConsoleApp/untitled/src/StudentManager.java
+++ ConsoleApp/untitled/src/StudentManager.java
@@ -3,6 +3,32 @@ import java.util.ArrayList;
 public class StudentManager {
     ArrayList<Student> students = new ArrayList<>();
 
+    public StudentManager(){
+        DateManager dateManager = new DateManager();
+
+        Student student1 = new Student("S3836322","Ngo My Quynh",dateManager.convertDateString("08/03/2001"));
+        Student student2 = new Student("S3817852","Nguyen Thanh Thien",dateManager.convertDateString("06/11/2000"));
+        Student student3 = new Student("S3836455","Nguyen Thanh Tuan",dateManager.convertDateString("14/12/1995"));
+        Student student4 = new Student("S3818102","Pham Cong Minh",dateManager.convertDateString("05/05/2001"));
+        Student student5 = new Student("S3818381","Vo Tran Truong Duy",dateManager.convertDateString("08/11/2001"));
+        Student student6 = new Student("S3754105","Nguyen Dang Lam Phuong",dateManager.convertDateString("25/07/2000"));
+        Student student7 = new Student("S3812649","Nguyen Pham Quoc Minh",dateManager.convertDateString("24/08/2001"));
+        Student student8 = new Student("S3740853","Vu Phuong Thao",dateManager.convertDateString("18/09/1999"));
+        Student student9 = new Student("S3804690","Nguyen Ngoc Dang Hung",dateManager.convertDateString("20/08/2000"));
+        Student student10 = new Student("S3689902","Nguyen Cao Minh Duc",dateManager.convertDateString("21/02/1999"));
+
+        this.students.add(student1);
+        this.students.add(student2);
+        this.students.add(student3);
+        this.students.add(student4);
+        this.students.add(student5);
+        this.students.add(student6);
+        this.students.add(student7);
+        this.students.add(student8);
+        this.students.add(student9);
+        this.students.add(student10);
+    }
+
     public ArrayList<Student> getStudents() {
         return students;
     }
@@ -21,15 +47,18 @@ public class StudentManager {
         while (flag == 0){
             System.out.print("Please enter the student id: ");
             studentId = Main.scanner.nextLine();
+            System.out.println(studentId);
 
-            for (Student student : students){
+            System.out.println(this.toString());
+
+            for (Student student : this.students){
                 if (student.getId().equals(studentId)) {
                     flag = 1;
                     break;
                 }
             }
 
-            if(flag != 1){
+            if(flag == 0){
                 System.out.println("The student ID is not recognized by the system");
                 System.out.println("Please try again");
             }
@@ -47,6 +76,10 @@ public class StudentManager {
         return null;
 
     }
+
+//    public void listOfStudent(){
+//
+//    }
 
     @Override
     public String toString() {
