01c1ace44710b6951d2432d14fa4962f27991172
Quynh <ngoquynh8383@gmail.com>
2021-03-21 18:24:21 +0700

    Making some modified for the main menu and the update enrollment menu

--- samples-java/Accounts/untitled/src/Main.java
+++ samples-java/Accounts/untitled/src/Main.java
@@ -9,36 +9,59 @@ public class Main {
         // Creating the object to later use the add, update,etc methods
         StudentEnrollment studentEnrollment = new StudentEnrollment();
 
-        // Making the interface menu
-        System.out.println("--------------MAIN MENU---------------");
-        System.out.println("1. Enroll a student for 1 semester");
-        System.out.println("2. Update the enrollment for 1 semester");
-        System.out.println("3. Get courses by student in 1 semester");
-        System.out.println("4. Get all the students by course in 1 semester");
-        System.out.println("5. Get all the course in 1 semester");
-        System.out.println("---------------------------------------");
-        String input = Main.scanner.next();
-        if (input.equals("1")){
-            studentEnrollment.askingInfo();
-        } else if (input.equals("2")) {
-            System.out.println("UPDATE ENROLLMENT MENU");
-            System.out.println("1. Adding a new course for student");
-            System.out.println("2. Delete a new course for student");
-            System.out.print("Your option: ");
-            input = Main.scanner.next();
-            if (input.equals("1")){
-                studentEnrollment.add();
-            } else {
-                studentEnrollment.delete();
-            }
-        } else if (input.equals("3")){
-            studentEnrollment.update();
-        } else if (input.equals("4")){
-            studentEnrollment.getStudentByCourse();
-        } else {
-            studentEnrollment.getCoursesBySemester();
-        }
+        while (true){
 
+            // Making the interface of main menu
+            System.out.println("MAIN MENU");
+            System.out.println("1. Enroll a student for 1 semester");
+            System.out.println("2. Update the enrollment for 1 semester");
+            System.out.println("3. Get courses by student in 1 semester");
+            System.out.println("4. Get all the students by course in 1 semester");
+            System.out.println("5. Get all the course in 1 semester");
+            System.out.println("6. Exit");
+
+            // Get the option from user
+            System.out.print("Your option: ");
+            String input = Main.scanner.nextLine();
+            System.out.println("\n");
+
+            if (input.equals("1")){
+
+                // Call out the adding student enrollment methods
+                studentEnrollment.askingInfo();
+            } else if (input.equals("2")) {
+                while (true){
+
+                    // Making the interface for update enrollment menu
+                    System.out.println("UPDATE ENROLLMENT MENU");
+                    System.out.println("1. Adding a new course for student");
+                    System.out.println("2. Delete a new course for student");
+                    System.out.println("3. Return to main menu");
+                    System.out.print("Your option: ");
+
+                    // Get the input from the users
+                    input = Main.scanner.nextLine();
+                    System.out.println("\n");
+                    if (input.equals("1")){
+                        studentEnrollment.add();
+                    } else if (input.equals("2")) {
+                        studentEnrollment.delete();
+                    } else {
+                        // Break the while loop to return back to the main menu
+                        break;
+                    }
+                }
+            } else if (input.equals("3")){
+                studentEnrollment.update();
+            } else if (input.equals("4")){
+                studentEnrollment.getStudentByCourse();
+            } else if (input.equals("5")) {
+                studentEnrollment.getCoursesBySemester();
+            } else {
+                System.out.println("Thank you for using the system, have a good day");
+                break;
+            }
+        }
 
     }
 
