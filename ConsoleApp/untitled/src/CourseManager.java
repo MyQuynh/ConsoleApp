b181f4bbb20c4b9c521e932b71ab4dad4b787d74
Quynh <ngoquynh8383@gmail.com>
2021-03-21 23:40:50 +0700

    Making change in CourseManager.java: Adding the validCourse methods to check if the course is in the system and adding the getCourseById methods

--- ConsoleApp/untitled/src/CourseManager.java
+++ ConsoleApp/untitled/src/CourseManager.java
@@ -16,6 +16,37 @@ public class CourseManager {
         this.coursesList = coursesList;
     }
 
+    public String validCourse(){
+        int flag = 0;
+        String courseId = "";
+        while (flag == 0){
+            System.out.print("Please enter the course ID: ");
+            courseId = Main.scanner.nextLine();
+
+            for (Course course : coursesList){
+                if (course.getId().equals(courseId)) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if(flag != 1){
+                System.out.println("The course ID is not recognized by the system");
+                System.out.println("Please try again");
+            }
+        }
+        return courseId;
+    }
+
+    public Course getCourseById(String courseId){
+        for(Course course : coursesList){
+            if(course.getId().equals(courseId)){
+                return course;
+            }
+        }
+        return null;
+    }
+
     @Override
     public String toString() {
         return "CourseManager{" +
