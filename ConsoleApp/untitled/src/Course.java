a97f46b6ccd2ea73a52b6f840d861fbf3a4dc767
Quynh <ngoquynh8383@gmail.com>
2021-03-21 23:41:39 +0700

    Making change in Course.java: Adding the toString() methods

--- ConsoleApp/untitled/src/Course.java
+++ ConsoleApp/untitled/src/Course.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class Course {
     private String id;
     private String name;
     private int number_of_credits;
+
+    public Course(){}
 
     public Course(String id, String name, int number_of_credits) {
         this.id = id;
@@ -31,5 +35,15 @@ public class Course {
 
     public void setNumber_of_credits(int number_of_credits) {
         this.number_of_credits = number_of_credits;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", number_of_credits=" + number_of_credits +
+                '}';
     }
 }
