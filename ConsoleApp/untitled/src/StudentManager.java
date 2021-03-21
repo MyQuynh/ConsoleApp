c824a12a52a481d5d2c98a4c2d10542a8212f71b
Quynh <ngoquynh8383@gmail.com>
2021-03-21 23:40:09 +0700

    Making change in StudentManager.java: Adding the validStudent methods to check if the student is in the system and adding the getStudentById methods

--- ConsoleApp/untitled/src/StudentManager.java
+++ ConsoleApp/untitled/src/StudentManager.java
@@ -15,6 +15,39 @@ public class StudentManager {
         this.students.add(student);
     }
 
+    public String validStudent(){
+        int flag = 0;
+        String studentId = "";
+        while (flag == 0){
+            System.out.print("Please enter the student id: ");
+            studentId = Main.scanner.nextLine();
+
+            for (Student student : students){
+                if (student.getId().equals(studentId)) {
+                    flag = 1;
+                    break;
+                }
+            }
+
+            if(flag != 1){
+                System.out.println("The student ID is not recognized by the system");
+                System.out.println("Please try again");
+            }
+        }
+        return studentId;
+    }
+
+    public Student getStudentById(String studentId){
+
+        for(Student student : students){
+            if(student.getId().equals(studentId)){
+                return student;
+            }
+        }
+        return null;
+
+    }
+
     @Override
     public String toString() {
         return "StudentManager{" +
