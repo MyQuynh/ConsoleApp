0791362db8b6faa84a630d5a08c7949c4c7f99d5
Quynh <ngoquynh8383@gmail.com>
2021-03-22 23:08:54 +0700

    StudentManager.java: Modified some small change

--- ConsoleApp/untitled/src/StudentManager.java
+++ ConsoleApp/untitled/src/StudentManager.java
@@ -6,16 +6,16 @@ public class StudentManager {
     public StudentManager(){
         DateManager dateManager = new DateManager();
 
-        Student student1 = new Student("S3836322","Ngo My Quynh",dateManager.convertDateString("08/03/2001"));
-        Student student2 = new Student("S3817852","Nguyen Thanh Thien",dateManager.convertDateString("06/11/2000"));
-        Student student3 = new Student("S3836455","Nguyen Thanh Tuan",dateManager.convertDateString("14/12/1995"));
-        Student student4 = new Student("S3818102","Pham Cong Minh",dateManager.convertDateString("05/05/2001"));
-        Student student5 = new Student("S3818381","Vo Tran Truong Duy",dateManager.convertDateString("08/11/2001"));
-        Student student6 = new Student("S3754105","Nguyen Dang Lam Phuong",dateManager.convertDateString("25/07/2000"));
-        Student student7 = new Student("S3812649","Nguyen Pham Quoc Minh",dateManager.convertDateString("24/08/2001"));
-        Student student8 = new Student("S3740853","Vu Phuong Thao",dateManager.convertDateString("18/09/1999"));
-        Student student9 = new Student("S3804690","Nguyen Ngoc Dang Hung",dateManager.convertDateString("20/08/2000"));
-        Student student10 = new Student("S3689902","Nguyen Cao Minh Duc",dateManager.convertDateString("21/02/1999"));
+        Student student1 = new Student("S3836322","Ngo My Quynh",dateManager.convertStringToDate("08/03/2001"));
+        Student student2 = new Student("S3817852","Nguyen Thanh Thien",dateManager.convertStringToDate("06/11/2000"));
+        Student student3 = new Student("S3836455","Nguyen Thanh Tuan",dateManager.convertStringToDate("14/12/1995"));
+        Student student4 = new Student("S3818102","Pham Cong Minh",dateManager.convertStringToDate("05/05/2001"));
+        Student student5 = new Student("S3818381","Vo Tran Truong Duy",dateManager.convertStringToDate("08/11/2001"));
+        Student student6 = new Student("S3754105","Nguyen Dang Lam Phuong",dateManager.convertStringToDate("25/07/2000"));
+        Student student7 = new Student("S3812649","Nguyen Pham Quoc Minh",dateManager.convertStringToDate("24/08/2001"));
+        Student student8 = new Student("S3740853","Vu Phuong Thao",dateManager.convertStringToDate("18/09/1999"));
+        Student student9 = new Student("S3804690","Nguyen Ngoc Dang Hung",dateManager.convertStringToDate("20/08/2000"));
+        Student student10 = new Student("S3689902","Nguyen Cao Minh Duc",dateManager.convertStringToDate("21/02/1999"));
 
         this.students.add(student1);
         this.students.add(student2);
