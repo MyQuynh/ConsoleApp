8f095b65be04ca48014eaf0946c237525bd9ae5f
Quynh <ngoquynh8383@gmail.com>
2021-03-22 23:09:31 +0700

    DateManager.java: Adding the convertDateToString()

--- ConsoleApp/untitled/src/DateManager.java
+++ ConsoleApp/untitled/src/DateManager.java
@@ -1,10 +1,11 @@
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateManager {
 
-    public Date convertDateString(String sDate1){
+    public Date convertStringToDate(String sDate1){
         SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
         Date date;
         try {
@@ -15,6 +16,14 @@ public class DateManager {
         }
         return null;
 
+    }
+
+    public String convertDateToString(Date date){
+
+        //DateFormat inputFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+        DateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        return outputFormat.format(date);
     }
 
 }
